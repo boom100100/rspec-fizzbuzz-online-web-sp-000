4b95a61ae8ce7a9f415264c0b2a1988a4a9ef0eb
Bernadette Davis <bsd231@nyu.edu>
2019-05-06 18:59:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+=begin
+'returns "Fizz" when the number is divisible by 3' do
+ expect(fizz_3).to eq("Fizz")
+
+'returns "Buzz" when the number is divisible by 5' do
+expect(fizz_5).to eq("Buzz")
+
+'returns "FizzBuzz" when the number is divisible by 3 and 5' do
+expect(fizz_15).to eq("FizzBuzz")
+
+'returns nil when the number is not divisible by 3 or 5' do
+expect(fizz_4).to eq(nil)
+=end
